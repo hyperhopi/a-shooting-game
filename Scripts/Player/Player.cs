005475f2e43c30b1159d1de7ab3878801636643d
hyperhopi <hyewon.park@hyperisland.se>
2023-07-20 01:11:01 +0200

    setup a player and add an enemy

---
+++ Scripts/Player/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health = 3;
+
+    void Start()
+    {
+        
+    }
+
+    void collidedWithEnemy(Enemy enemy)
+    {
+        enemy.Attack(this);
+
+        if (health <= 0)
+        {
+            // Todo 
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Enemy enemy = col.collider.gameObject.GetComponent<Enemy>();
+        collidedWithEnemy(enemy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
