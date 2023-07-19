ce773b851ebbe5b2d4985b4289df2dba415deab4
hyperhopi <hyewon.park@hyperisland.se>
2023-07-20 01:33:04 +0200

    create projectiles and shooting function

---
+++ Scripts/Projectile.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public int damage;
+
+    Vector3 shootDirection;
+
+    void Start()
+    {
+        
+    }
+
+    // 1
+    void FixedUpdate()
+    {
+        this.transform.Translate(shootDirection * speed, Space.World);
+    }
+
+    // 2
+    public void FireProjectile(Ray shootRay)
+    {
+        this.shootDirection = shootRay.direction;
+        this.transform.position = shootRay.origin;
+        rotateInShootDirection();
+    }
+
+    // 3
+    void OnCollisionEnter(Collision col)
+    {
+        Enemy enemy = col.collider.gameObject.GetComponent<Enemy>();
+        if (enemy)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(this.gameObject);
+    }
+
+    void rotateInShootDirection()
+    {
+        Vector3 newRotation = Vector3.RotateTowards(transform.forward, shootDirection, 0.01f, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newRotation);
+    }
+}
